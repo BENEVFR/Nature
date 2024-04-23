5b2536098d806e6ad2b8581956f1b13bcdc97b66
Arthur Bréant <arthur@thinkr.fr>
2024-04-23 17:36:15 +0200

    chore: create data inside the package with questions

---
+++ R/doc_survey_questions.R
@@ -0,0 +1,11 @@
+#' survey_questions
+#'
+#' All questions from the survey
+#'
+#' @format A data frame with 97 rows and 2 variables:
+#' \describe{
+#'   \item{ code }{  character }
+#'   \item{ question }{  character }
+#' }
+#' @source Source
+"survey_questions"
