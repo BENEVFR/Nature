39e91efa5c193f4bb20a76a7907620ab43e832bd
Arthur Bréant <arthur@thinkr.fr>
2024-05-13 10:42:32 +0200

    feat: optimize JS code to create new map

--- dev/creteil/map.js
+++ dev/creteil/map.js
@@ -1,66 +1,39 @@
-var map;
-var map2;
-var map3;
-var geojsonLayerCreteil;
+var carte1;
+var carte2;
+var carte3;
 
-function redraw_map(property) {
+function createMap(idElement, geodata_city, geodata_hex, property, zoom = 13) {
 
-  if (map) {
-    map.remove();
-  }
+  var map = L.map(idElement).setView([48.791100, 2.462800], zoom);
 
-  if (map2) {
-    map2.remove();
-  }
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+  }).addTo(map);
 
-  if (map3) {
-    map3.remove();
-  }
-
-
-  map = L.map('map').setView([48.791100, 2.462800], 13); // Initialisation de la carte
-  map2 = L.map('map2').setView([48.791100, 2.462800], 13); // Initialisation de la carte
-  map3 = L.map('map3').setView([48.791100, 2.462800], 13); // Initialisation de la carte
-
-  L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
-    }
-  ).addTo(map);
-
-  L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
-    }
-  ).addTo(map2);
-
-    L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
-    }
-  ).addTo(map3);
-
-
-
-  geojsonLayerCreteil = L.geoJson(geodata_creteil, {
-
+  geojsonCityLayer = L.geoJson(geodata_city, {
     style: function(feature) {
       return {
         color: '#000000',
         fillColor: "#ffffff",
         fillOpacity: 0.5
       };
-    },
-
+    }
   }).addTo(map);
 
-  var geojsonLayer = L.geoJson(geodata_hex, {
+  geojsonHexLayer = drawHex(geodata_hex, property).addTo(map);
 
+  return {
+    map: map,
+    geojsonCityLayer: geojsonCityLayer,
+    geojsonHexLayer: geojsonHexLayer
+  };
+}
+
+function drawHex(geodata_hex, property) {
+
+   geojsonHexLayer = L.geoJson(geodata_hex, {
     style: function(feature) {
-
       var color_property = property + '_color';
       var color = feature.properties[color_property];
 
@@ -70,10 +43,9 @@ function redraw_map(property) {
         opacity: 0.5,
         color: 'white',
         fillOpacity: 0.3,
-        transition: 'fill-opacity 0.3s' // Ajout de transition
+        transition: 'fill-opacity 0.3s'
       };
     },
-
     onEachFeature: function(feature, layer) {
       if (feature.properties) {
         var popupContent = "<p><strong>ID:</strong> " + feature.properties.id + "</p>" + "<p><strong>" + property + ":</strong> " + feature.properties[property] + "</p>";
@@ -89,18 +61,25 @@ function redraw_map(property) {
         });
       }
     }
-  }).addTo(map);
+  });
 
+  return geojsonHexLayer;
 }
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM is Ready!");
 
-  redraw_map('critere_bien_etre');
+  carte1 = createMap("map", geodata_creteil, geodata_hex, "critere_bien_etre");
+  carte2 = createMap("map2", geodata_creteil, geodata_hex, "critere_bien_etre");
+  carte3 = createMap("map3", geodata_creteil, geodata_hex, "critere_bien_etre");
 
   var elt = document.getElementsByClassName("nav-link")
   for (let index = 0; index < elt.length; index++) {
-      elt[index].addEventListener("click", function() {window.dispatchEvent(new Event('resize'));})
+    elt[index].addEventListener("click", function() {
+      window.dispatchEvent(new Event('resize'));
+    });
   }
 });
 
@@ -108,13 +87,25 @@ function select_critere_change() {
   var x = document.getElementById("my_select").value;
   console.log(x);
 
-  redraw_map(x);
-  document.getElementById("hidemapinfos").checked = false;
+  updateProperty(x);
 };
+
+function updateProperty(property) {
+  carte1.geojsonHexLayer.removeFrom(carte1.map);
+  carte2.geojsonHexLayer.removeFrom(carte2.map);
+  carte3.geojsonHexLayer.removeFrom(carte3.map);
+
+  carte1.geojsonHexLayer = drawHex(geodata_hex, property).addTo(carte1.map);
+  carte2.geojsonHexLayer = drawHex(geodata_hex, property).addTo(carte2.map);
+  carte3.geojsonHexLayer = drawHex(geodata_hex, property).addTo(carte3.map);
+}
 
 function changeOpacity() {
   var switchState = document.getElementById("hidemapinfos").checked;
   var opacityValue = switchState ? 1 : 0.5;
-  geojsonLayerCreteil.setStyle({ fillOpacity: opacityValue });
+
+  carte1.geojsonCityLayer.setStyle({ fillOpacity: opacityValue });
+  carte2.geojsonCityLayer.setStyle({ fillOpacity: opacityValue });
+  carte3.geojsonCityLayer.setStyle({ fillOpacity: opacityValue });
 }
 
