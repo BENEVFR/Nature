f29f147e01d6b8ad51554c6678d3d91f65a5687c
Arthur Bréant <arthur@thinkr.fr>
2024-05-23 08:57:13 +0200

    fix: should maps reused their own data

--- inst/templates/html_report/map.js
+++ inst/templates/html_report/map.js
@@ -9,30 +9,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
   /* */
   /*Ajoutez des cartes dans le rapport*/
   /* */
+
   createMap(
     idElement = "map",
-    geodata_city = geodata_creteil,
-    geodata_hex = geodata_hex,
-    property = "critere_bien_etre_global",
-    lat = latitude,
-    long = longitude
+    geodata_city = geodata_city, /*Pas besoin de modifier*/
+    geodata_hex = geodata_dataset,
+    property = "critere_bien_etre_global", /*Pas besoin de modifier*/
+    lat = latitude, /*Pas besoin de modifier*/
+    long = longitude /*Pas besoin de modifier*/
   );
 
   createMap(
     idElement = "map2",
-    geodata_city = geodata_creteil,
-    geodata_hex = geodata_hex,
-    property = "critere_bien_etre_global",
-    lat = latitude,
-    long = longitude
+    geodata_city = geodata_city, /*Pas besoin de modifier*/
+    geodata_hex = geodata_dataset_souspop,
+    property = "critere_bien_etre_global", /*Pas besoin de modifier*/
+    lat = latitude, /*Pas besoin de modifier*/
+    long = longitude /*Pas besoin de modifier*/
   );
 
   createMap(
     idElement = "map3",
-    geodata_city = geodata_creteil,
-    geodata_hex = geodata_hex,
-    property = "critere_bien_etre_global",
-    lat = latitude,
-    long = longitude
+    geodata_city = geodata_city, /*Pas besoin de modifier*/
+    geodata_hex = geodata_dataset_souspop2,
+    property = "critere_bien_etre_global", /*Pas besoin de modifier*/
+    lat = latitude, /*Pas besoin de modifier*/
+    long = longitude /*Pas besoin de modifier*/
   );
 });
