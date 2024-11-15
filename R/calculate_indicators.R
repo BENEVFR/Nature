d6e53d14d2d3223227580a5ba7bff4f22a434542
Ju <ju@MBP-de-Daphne-001.lan>
2024-11-15 12:03:38 +0100

    Modif Ratio

--- R/calculate_indicators.R
+++ R/calculate_indicators.R
@@ -3,7 +3,6 @@
 #' @param data answers from survey
 #'
 #' @importFrom dplyr group_by ungroup mutate row_number rowwise select starts_with across
-#' @importFrom BBmisc normalize
 #'
 #' @return a tibble
 #' @export
@@ -18,16 +17,16 @@ calculate_indicators <- function(data) {
     mutate(
       critere_bien_etre_global = mean(
         c(BE3PS, BE4SP, BE10PH, BE15PH,
-        BE17PH, BE18PS, BE19RS, BE20RS,
-        BE26PSR, BE7ENV, BE8ENV, BE22ENV,
-        BE1PH, BE2PH, BE14PH, BE28G, BE11ENV,
-        BE12ENV, BE13ENV, BE9PH),
+          BE17PH, BE18PS, BE19RS, BE20RS,
+          BE26PSR, BE7ENV, BE8ENV, BE22ENV,
+          BE1PH, BE2PH, BE14PH, BE28G, BE11ENV,
+          BE12ENV, BE13ENV, BE9PH),
         na.rm = TRUE
       ),
       critere_bien_etre_psy = mean(
         c(BE3PS, BE4SP, BE10PH,
-        BE15PH, BE17PH, BE18PS,
-        BE19RS, BE20RS, BE26PSR),
+          BE15PH, BE17PH, BE18PS,
+          BE19RS, BE20RS, BE26PSR),
         na.rm = TRUE
       ),
       critere_bien_etre_environnement = mean(
@@ -44,12 +43,12 @@ calculate_indicators <- function(data) {
       ),
       critere_type_nature_global = mean(
         c(CN1, CN2, CN11, CN15, CN5, CN6, CN7,
-        CN8, CN14, CN16, CN18, CN19),
+          CN8, CN14, CN16, CN18, CN19),
         na.rm = TRUE
       ),
       critere_activ_global = mean(
         c(ACT1, ACT2, ACT3, ACT4, ACT5, ACT8,
-        ACT9, ACT6, ACT7),
+          ACT9, ACT6, ACT7),
         na.rm = TRUE
       ),
       critere_nature_nondomest = mean(
@@ -80,26 +79,24 @@ calculate_indicators <- function(data) {
         c(CP1, CP2, CP3, CP4, CP5, CP6, CP7, CP8, CP9, CP10),
         na.rm = TRUE
       ),
-       critere_biodiv_global = mean(
-         c(BIO1, BIO2, BIO3, BIO4, BIO5, BIO6, BIO7),
-         na.rm = TRUE
-       )
+      critere_biodiv_global = mean(
+        c(BIO1, BIO2, BIO3, BIO4, BIO5, BIO6, BIO7),
+        na.rm = TRUE
+      )
     ) |>
     ungroup()
 
   Sc_max_B_E <- 5 # max(result$critere_bien_etre_global)
   Sc_max_Nature <- 5 # max(result$critere_type_nature_global)
-  dispersion_bien_etre = sd(result$critere_bien_etre_global, na.rm = TRUE)
-  dispersion_nature = sd(result$critere_type_nature_global, na.rm = TRUE)
 
   result <- result |>
-      rowwise() |>
-      mutate(
-        critere_ratio_bien_etre_nature = (mean(critere_bien_etre_global, na.rm = TRUE) * mean(critere_type_nature_global, na.rm = TRUE)) - 0.2 * (dispersion_bien_etre + dispersion_nature),
-        across(
-          starts_with("critere"),
-          ~ round(.x, digits = 3)
-          )
+    rowwise() |>
+    mutate(
+      critere_ratio_bien_etre_nature = ((Sc_max_B_E - critere_bien_etre_global) * (Sc_max_Nature - critere_type_nature_global)) / 16,
+      across(
+        starts_with("critere"),
+        ~ round(.x, digits = 3)
+      )
     ) |>
     ungroup() |>
     select(
