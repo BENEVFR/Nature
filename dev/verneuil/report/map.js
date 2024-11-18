9a97191c3ff1f16e158544726780bdf7c4c1d7e6
Ju <ju@MBP-de-Daphne.lan>
2024-11-18 09:47:43 +0100

    remplacement Lat Long

--- dev/verneuil/report/map.js
+++ dev/verneuil/report/map.js
@@ -3,8 +3,8 @@ var cartes = [];
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("Maps are Ready!");
 
-  const latitude = 48.98714060051876; /*Remplacez ici la latitude de la ville*/
-  const longitude = 1.964095198461945;  /*Remplacez ici la longitude de la ville*/
+  const latitude = 48.985335; /*Remplacez ici la latitude de la ville*/
+  const longitude = 1.963700;  /*Remplacez ici la longitude de la ville*/
 
   /* */
   /*Ajoutez des cartes dans le rapport*/
@@ -19,4 +19,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     long = longitude /*Pas besoin de modifier*/
   );
 
+  createMap(
+    idElement = "map2",
+    geodata_city = geodata_city, /*Pas besoin de modifier*/
+    geodata_hex = geodata_dataset_souspop,
+    property = "critere_bien_etre_global", /*Pas besoin de modifier*/
+    lat = latitude, /*Pas besoin de modifier*/
+    long = longitude /*Pas besoin de modifier*/
+  );
+
+  createMap(
+    idElement = "map3",
+    geodata_city = geodata_city, /*Pas besoin de modifier*/
+    geodata_hex = geodata_dataset_souspop2,
+    property = "critere_bien_etre_global", /*Pas besoin de modifier*/
+    lat = latitude, /*Pas besoin de modifier*/
+    long = longitude /*Pas besoin de modifier*/
+  );
 });
