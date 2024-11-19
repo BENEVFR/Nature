319f9e5f9d6a53aa450044853fccc931b93a6e62
Ju <ju@MBP-de-Daphne.lan>
2024-11-19 15:08:23 +0100

    Modif message accueil

--- dev/verneuil/survey/script.js
+++ dev/verneuil/survey/script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     html: `
 Mesdames, Messieurs, <br>La végétalisation et la préservation de la nature sont un axe prioritaire du développement de la ville. Afin d’évaluer les résultats des actions menées et prioriser les actions futures, nous vous remercions de bien vouloir répondre à cette enquête portant sur votre bien-être et votre perception de la nature à proximité de votre domicile. Cette enquête est réalisée avec l’aide de deux chercheuses et est entièrement anonyme.
 
+Si vous acceptez de participer à notre enquête, merci de cliquer sur "J'ai compris" puis de localiser votre lieu de résidence sur la carte qui s'affiche.
+
 Vous remerciant pour votre contribution,<br>
 Bien cordialement,<br>
 Votre maire
